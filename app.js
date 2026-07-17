6c8fd09c9db4fd0fdf233b5a149596ba05953df3
ziczac309 <bhavydeshmuk@gmail.com>
2026-07-17 21:36:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const slides = document.querySelectorAll(".slide");
 
+let index = 0;
+
+function slider() {
+
+slides[index].classList.remove("active");
+
+index++;
+
+if(index >= slides.length){
+index = 0;
+}
+
+slides[index].classList.add("active");
+
+}
+
+setInterval(slider,3000);
