a967275dd3b85cb19bfb73fd145732a037284ce0
ziczac309 <bhavydeshmuk@gmail.com>
2026-07-17 22:20:23 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "APNI_API_KEY",
+  authDomain: "gamer-esports-ada07.firebaseapp.com",
+  projectId: "gamer-esports-ada07",
+  storageBucket: "gamer-esports-ada07.appspot.com",
+  messagingSenderId: "914920329816",
+  appId: "APNA_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
